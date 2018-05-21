bc9ad3629212eae742826657ee3b9e934bbba6f1
Tom Peri <tomp228@gmail.com>
2018-05-21 19:22:44 +0300

    New frame prefab

---
+++ Project/Assets/NewDoor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NewDoor : MonoBehaviour
+{
+    public bool doorOpen;
+    public DoorColor color;
+
+    NavMeshObstacle obstacle;
+    private Collider collider;
+
+    void Start()
+    {
+        obstacle = GetComponent<NavMeshObstacle>();
+        collider = GetComponent<Collider>();
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        if (other.gameObject.tag == "Door" && color == other.GetComponentInParent<NewDoor>().color)
+        {
+            OpenDoors();
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        Debug.Log("Exit");
+        CloseDoors();
+    }
+
+    public void OpenDoors()
+    {
+        doorOpen = true;
+        obstacle.enabled = false;
+    }
+
+    public void CloseDoors()
+    {
+        doorOpen = false;
+        obstacle.enabled = true;
+    }
+}
