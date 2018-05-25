a99f927050fba4f7bab9dcc9eb9dc1bbc60ff8e1
Amir Padan <amirpadan@gmail.com>
2018-05-25 22:27:00 +0300

    Implemented basic floating stone destruction

--- Project/Assets/Scripts/Controllers/FrameAttachable.cs
+++ Project/Assets/Scripts/Controllers/FrameAttachable.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class FrameAttachable : MonoBehaviour {
 
-    private FrameManager frameManager;
-    private Frame frameController;
-
-    private void Start()
-    {
-        frameManager = FindObjectOfType<FrameManager>();
-        frameController = GetComponent<Frame>();
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" || other.tag == "Projectile")
         {
             PlaceObjectUnderParent(other.gameObject);
-            Debug.Log("Grandma moved to " + transform.parent.gameObject.name);
+            Debug.Log(other.tag + " moved to " + transform.parent.gameObject.name);
 
         }
     }
