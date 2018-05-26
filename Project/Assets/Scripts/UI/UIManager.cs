5e2215b8b482166b251abfc1618c3353adb70589
Tom Peri <tomp228@gmail.com>
2018-05-27 00:27:42 +0300

    Pause menu prefab + UIManager script

---
+++ Project/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public bool isPause = false;
+    public GameObject PauseMenu;
+
+    public static void GoToLevel(int id)
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + id;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public static void GoToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void RestartCurrentLevel()
+    {
+
+    }
+
+    public void DisplayPauseMenu(bool i_Display)
+    {
+        PauseMenu.SetActive(i_Display);
+    }
+
+}
