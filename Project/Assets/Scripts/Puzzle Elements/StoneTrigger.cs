6cd904fb2080da7fd2d7fbca34e531a8aec228c1
ndanieli <ndanieli@gmail.com>
2018-05-26 18:17:56 +0300

    updated projectile trigger image

--- Project/Assets/Scripts/Puzzle Elements/StoneTrigger.cs
+++ Project/Assets/Scripts/Puzzle Elements/StoneTrigger.cs
@@ -5,18 +5,56 @@ using UnityEngine;
 public class StoneTrigger : MonoBehaviour {
 
     private ProjectileManager projectileManager;
+    private GameController gameController;
+
+    private GameObject offSprite;
+    private GameObject onSprite;
+    private bool isOff;
 
     private void Start()
     {
         projectileManager = FindObjectOfType<ProjectileManager>();
+        gameController = FindObjectOfType<GameController>();
+        offSprite = transform.GetChild(0).gameObject;
+        onSprite = transform.GetChild(1).gameObject;
+
+        isOff = true;
+        offSprite.SetActive(true);
+        onSprite.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Player"))
         {
-            projectileManager.SpawnProjectile();
+            StartCoroutine(waitAndDo());
         }
+    }
+
+    IEnumerator waitAndDo()
+    {
+
+        if (isOff)
+        {
+            isOff = false;
+            offSprite.SetActive(false);
+            onSprite.SetActive(true);
+        }
+        else
+        {
+            isOff = true;
+            offSprite.SetActive(true);
+            onSprite.SetActive(false);
+        }
+
+        yield return new WaitForSeconds(0.3f);
+
+        if (gameController.IsZoomedIn)
+        {
+            gameController.zoomInOut();
+        }
+
+        projectileManager.SpawnProjectile();
     }
 
 }
