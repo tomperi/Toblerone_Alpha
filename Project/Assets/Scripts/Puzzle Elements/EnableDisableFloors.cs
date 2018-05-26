7ba2a9f58c72a7073eeac3bacfe8a400b9f63fc7
ndanieli <ndanieli@gmail.com>
2018-05-26 17:04:07 +0300

    all levels build

--- Project/Assets/Scripts/Puzzle Elements/EnableDisableFloors.cs
+++ Project/Assets/Scripts/Puzzle Elements/EnableDisableFloors.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnableDisableFloors : MonoBehaviour {
+
     public GameObject[] gameObjects;
+    private GameController gameController;
+
+    private void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
-            foreach (GameObject go in gameObjects)
+            StartCoroutine(zoomOutAndSwitch());   
+        }
+    }
+
+    IEnumerator zoomOutAndSwitch()
+    {
+        if (gameController.IsZoomedIn)
+        {
+            gameController.zoomInOut();
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        foreach (GameObject go in gameObjects)
+        {
+            for (int i = 0; i < go.transform.childCount; i++)
             {
-                for (int i = 0; i < go.transform.childCount; i++)
-                {
-                    GameObject child = go.transform.GetChild(i).gameObject;
-                    child.SetActive(!child.activeSelf);
-                }
+                GameObject child = go.transform.GetChild(i).gameObject;
+                child.SetActive(!child.activeSelf);
             }
         }
     }
